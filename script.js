68e4337a9c87722dc1e4edbd355e546d3a77e520
Ivan Sanchez <isancr00@estudiantes.unileon.es>
2021-04-28 18:36:52 +0200

    Hint and know if completed not done

--- script.js
+++ script.js
@@ -14,11 +14,132 @@ function limpiarPasatiempo(){
 function acercaDe(){
     var pestania = window.open();
     pestania.document.open();
-    contenido = "<!DOCTYPE html><html><style>h1{text-align: center;font-family: monospace;}</style><head><title>Acerca de la pr&aacutectica</title></head><body><h1>PRACTICA PASATIEMPO EN CASCADA APLICACIONES WEB</h1><div><p>Esta es la Pr&aacutectica 1 correspondiente a la asignatura de Aplicaciones Web.</p><p>La pr&aacutectica consiste en la realizac&oacuten de un pasatiempo en cascada.</p><p>El pasatiempo contiene una serie de pistas correspondientes a ciertas palabras identificadas en el tablero como números. A partir de dichas palabras, se han de descubrir las palabras intermedias.Esto se consigue cambiando una letra de la palabra anterior, para así obtener la siguiente. Todas las palabras tienen que estar contenidas en el diccionario contenido en: <a href= ´https://ordenalfabetix.unileon.es/aw/diccionario.txt´>Diccionario</a></p></div></body></html>"
+    contenido = "<!DOCTYPE html><html><style>h1{text-align: center;font-family: monospace;}</style><head><title>Acerca de la pr&aacutectica</title></head><body><h1>PRACTICA PASATIEMPO EN CASCADA APLICACIONES WEB</h1><div><p>Esta es la Pr&aacutectica 1 correspondiente a la asignatura de Aplicaciones Web.</p><p>La pr&aacutectica consiste en la realizac&oacuten de un pasatiempo en cascada.</p><p>El pasatiempo contiene una serie de pistas correspondientes a ciertas palabras identificadas en el tablero como números. A partir de dichas palabras, se han de descubrir las palabras intermedias.Esto se consigue cambiando una letra de la palabra anterior, para así obtener la siguiente. Todas las palabras tienen que estar contenidas en el diccionario contenido en: <a href= https://ordenalfabetix.unileon.es/aw/diccionario.txt>Diccionario</a></p></div></body></html>"
     pestania.document.write(contenido);
     
 }
 
-function comprobarPalabra(){
-    document.getElementById("cookies").innerHTML += "Funcionando cielo"
+function comprobarPalabra(id){
+    var palabraActual = "";
+    var idString = String(id);
+    var estaCompleta = palabraCompleta(idString);
+    if(estaCompleta){
+        //Comprobamos que la palabra esté completa
+        palabraActual = construirPalabra(id);
+        estaEnDiccionario(palabraActual);
+
+    }
+
+}
+
+function construirPalabra(id){
+    var idString = id.toString();
+    var letraString = idString.charAt(5);
+    var palabraString = "";
+    palabraString += idString.charAt(7);
+
+    if(idString.charAt(8) != null){
+        palabraString += idString.charAt(8);
+    }
+
+    var letra = parseInt(letraString.toString());
+    var palabra = parseInt(palabraString.toString());
+    var palabraActual = ""
+    if(palabra < 7){
+        for(var i=0;i<letra;i++){
+            var palabraAux = "letra" + (i+1) + "P" + palabra;
+            palabraActual += document.getElementById(palabraAux.toString()).value;
+        }
+        
+        for(var i=letra;i<4;i++){
+            var palabraAux = "letra" + (i+1) + "P" + palabra
+            palabraActual += document.getElementById(palabraAux.toString()).value;
+        }
+    }
+    return palabraActual;
+}
+
+function estaEnDiccionario(palabraActual){
+    var texto;
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200){
+            texto = this.responseText.split("/n");
+
+            if(!texto.includes(palabraActual)){
+                alert("La palabra no está contenida en el diccionario")
+            }
+        }
+    };
+
+    xhr.open("GET", "https://ordenalfabetix.unileon.es/aw/diccionario.txt", true);
+    xhr.send();
+   
+}
+
+function guardarEstado(){
+    var cookies = document.getElementById("cookies").textContent;
+    if(cookies == "Almacenamiento no permitido"){
+        alert("El almacenamiento no ha sido permitido.");
+        preguntaCookies();
+    }else{
+        var form = document.getElementById("datos");
+
+        for(var i=0;i<form.length-1;i++){
+            localStorage.setItem(form[i].id, form[i].value);
+        }
+
+    }
+
+    console.log(localStorage);
+}
+
+function cargarEstado(){
+    var form = document.getElementById("datos");
+
+    for(var i=0;i<form.length-1;i++){
+        form[i].value = localStorage.getItem(form[i].id);
+    }
+}
+
+function limpiarAlmacenamiento(){
+    localStorage.clear();
+}
+
+function palabraCompleta(idString){
+    var completa = false;
+    var letraString = idString.charAt(5);
+    var palabraString = "";
+    palabraString += idString.charAt(7);
+
+    if(idString.charAt(8) != null){
+        palabraString += idString.charAt(8);
+    }
+
+    var letra = parseInt(letraString.toString());
+    var palabra = parseInt(palabraString.toString());
+
+    if(palabra < 7){
+        for(var i=0;i<letra;i++){
+            var palabraAux = "letra" + (i+1) + "P" + palabra;
+
+            if(document.getElementById(palabraAux.toString()).value != ""){
+                completa = true
+            }else{
+                return false;
+            }
+        }
+        
+        for(var i=letra;i<4;i++){
+            var palabraAux = "letra" + (i+1) + "P" + palabra
+
+            if(document.getElementById(palabraAux.toString()).value != ""){
+                completa = true;
+            }else{
+                return false;
+            }
+        }
+    }
+
+    return completa
 }
